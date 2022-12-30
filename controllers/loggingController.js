119030f8f53bcc55bea50e030518d2edfb599934
edouard <edouard.blais@hotmail.com>
2022-12-30 10:34:56 -0500

    sanitized registering form

--- controllers/loggingController.js
+++ controllers/loggingController.js
@@ -2,26 +2,59 @@
 exports.__esModule = true;
 var bcrypt = require("bcryptjs");
 var passport = require("passport");
+var express_validator_1 = require("express-validator");
 var user_1 = require("../models/user");
 exports.log_register_get = function (req, res) {
     res.render("signup");
 };
-exports.log_register_post = function (req, res, next) {
-    bcrypt.hash(req.body.password, 10, function (err, hashedPassword) {
-        if (err) {
-            return next(err);
+exports.log_register_post = [
+    (0, express_validator_1.body)("username", "Username required").trim().isLength({ min: 1 }).escape(),
+    (0, express_validator_1.body)('email', 'Email required').isEmail().normalizeEmail().escape(),
+    (0, express_validator_1.body)('password', 'Password of minimally 8 characters required').trim().isLength({ min: 8 }).escape(),
+    (0, express_validator_1.body)('confirmpassword', 'Password must be the same as above').custom(function (value, _a) {
+        var req = _a.req;
+        return value === req.body.password;
+    }),
+    function (req, res, next) {
+        // Extract the validation errors from a request.
+        var errors = (0, express_validator_1.validationResult)(req);
+        if (!errors.isEmpty()) {
+            res.render("signup", { errors: errors.array() });
         }
-        var user = new user_1["default"]({
-            username: req.body.username,
-            password: hashedPassword
-        }).save(function (err) {
-            if (err) {
-                return next(err);
-            }
-            res.redirect("/");
-        });
-    });
-};
+        else {
+            // Hashing password
+            bcrypt.hash(req.body.password, 10, function (err, hashedPassword) {
+                if (err) {
+                    return next(err);
+                }
+                // creating user
+                var user = new user_1["default"]({
+                    username: req.body.username,
+                    password: hashedPassword
+                });
+                // Check if User with same username already exists.
+                user_1["default"].findOne({ name: user.username }).exec(function (err, found_user) {
+                    if (err) {
+                        return next(err);
+                    }
+                    if (found_user) {
+                        // username already exists.
+                        res.render("signup", { errors: ['Username already exists'] });
+                    }
+                    else {
+                        user.save(function (err) {
+                            if (err) {
+                                return next(err);
+                            }
+                            res.redirect("/");
+                        });
+                    }
+                    ;
+                });
+            });
+        }
+    }
+];
 exports.log_signin_get = function (req, res) {
     res.render("signin");
 };
--- controllers/loggingController.ts
+++ controllers/loggingController.ts
@@ -1,28 +1,63 @@
 import { Request, Response, NextFunction } from "express";
 import * as bcrypt from "bcryptjs";
 import * as passport from "passport";
+import { body, validationResult } from 'express-validator';
 import User from "../models/user";
 
 exports.log_register_get = (req: Request, res: Response) => {
   res.render("signup");
 };
 
-exports.log_register_post = (req: Request, res: Response, next: NextFunction) => {
-    bcrypt.hash(req.body.password, 10, (err: Error, hashedPassword: string) => {
+exports.log_register_post = [
+
+  body("username", "Username required").trim().isLength({ min: 1 }).escape(),
+  
+  body('email', 'Email required').isEmail().normalizeEmail().escape(),
+  
+  body('password', 'Password of minimally 8 characters required').trim().isLength({ min: 8 }).escape(),
+
+  body('confirmpassword', 'Password must be the same as above').custom((value, { req }) => value === req.body.password),
+  
+  (req: Request, res: Response, next: NextFunction) => {
+
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render("signup", {errors: errors.array()})
+
+    } else {
+      // Hashing password
+      bcrypt.hash(req.body.password, 10, (err: Error, hashedPassword: string) => {
         if (err) { 
             return next(err);
         }
+        // creating user
         const user = new User({
             username: req.body.username,
             password: hashedPassword,
-          }).save((err: Error) => {
-            if (err) { 
-              return next(err);
-            }
+        })
+        // Check if User with same username already exists.
+        User.findOne({ name: user.username}).exec((err: Error, found_user: string) => {
+          if (err) {
+            return next(err);
+          }
+
+          if (found_user) {
+            // username already exists.
+            res.render("signup", {errors: ['Username already exists']});
+          } else {
+            user.save((err: Error) => {
+              if (err) { 
+                return next(err);
+              }
             res.redirect("/");
-          });
-      });
-};
+          })};
+        })
+      })
+    }
+  }
+];
 
 exports.log_signin_get = (req: Request, res: Response) => {
     res.render("signin");
